d9d7fe5961b1df592a7870891ad7895d24d73d3e
Clyde W <gomideshi@gmail.com>
2014-11-03 20:55:32 -0600

    Read Source Change
    demo.py now reads from an external .csv file (readsample.csv) and has
    discarded the StringIO import.

--- demo.py
+++ demo.py
@@ -1,23 +1,15 @@
-data = \
-"""
-Demo,Demo,Jim
-Demo,Demo,John
-Demo,Demo,John
-Demo,Demo,Jake
-Demo,Demo,Jake
-Demo,Demo,John
-"""
+#Read the file line by line
+#Append the values for location to a list
+#Build a set of uniques from that list
+#Determine the count for each of the uniques in the list
 
 import csv
-import StringIO
 from collections import Counter
 
+with open('readsample.csv', 'rb') as f:
+    reader = csv.reader(f)
+    reader.next()
+    names = [row[2] for row in reader]
 
-input_stream = StringIO.StringIO(data)
-reader = csv.reader(input_stream, delimiter=',')
-
-reader.next()
-cities = [row[2] for row in reader]
-
-for (k,v) in Counter(cities).iteritems():
-    print "%s appears %d times" % (k, v)
+    for (k,v) in Counter(names).iteritems():
+        print "%s appears %d times" % (k, v)
