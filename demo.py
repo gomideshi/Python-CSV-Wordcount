b4606feffd072021d51584eec8ae67e654c5714c
Clyde W. <gomideshi@gmail.com>
2014-11-04 01:15:33 -0600

    Second Iteration
    Overhauled demo.py - now accurately pulls the contents of all cells
    within a csv delimited by commas, counts the input, and outputs the top
    25 results.

--- demo.py
+++ demo.py
@@ -1,15 +1,28 @@
-#Read the file line by line
-#Append the values for location to a list
-#Build a set of uniques from that list
-#Determine the count for each of the uniques in the list
+#--------------------------------------------------------------------
+# TODO:
+#   - lowercase() the contents of an early data set to prevent dupes
+#   - filter (re.sub?) non alpha-numeric from early data set.
+#   - consider input/raw_input for file selection/needed top X
+#--------------------------------------------------------------------
 
-import csv
-from collections import Counter
+import csv, itertools
 
-with open('readsample.csv', 'rb') as f:
-    reader = csv.reader(f)
-    reader.next()
-    names = [row[2] for row in reader]
+#Read the contents of 'readsample.csv' and begin splitting it
+incoming_data = open('readsample.csv', 'r').read().split()
+delimited_data = [item.split(',') for item in incoming_data]
 
-    for (k,v) in Counter(names).iteritems():
-        print "%s appears %d times" % (k, v)
+#Recombine the contents of the list into one meta list
+remerged = list(itertools.chain(*delimited_data))
+
+#Count the frequency of each word, and pair it as a dict entry
+wordfreq = [remerged.count(p) for p in remerged]
+dictionary = dict(zip(remerged, wordfreq))
+
+#For each pair, sort by key (wordfreq) and reverse to top-down
+aux = [(dictionary[key], key) for key in dictionary]
+aux.sort()
+aux.reverse()
+
+#For the top 25 entries, print them
+for a in aux[:25]:
+    print a
