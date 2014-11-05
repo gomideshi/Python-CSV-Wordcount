20036e42175b1827c05d2a1807bf0d935716e300
Clyde W. <gomideshi@gmail.com>
2014-11-05 00:22:31 -0600

    Input Change
    Now prompting for filename / desired list size. Renamed main file.

--- wordcount.py
+++ wordcount.py
@@ -1,14 +1,17 @@
 #--------------------------------------------------------------------
 # TODO:
 #   - lowercase() the contents of an early data set to prevent dupes
-#   - filter (re.sub?) non alpha-numeric from early data set.
-#   - consider input/raw_input for file selection/needed top X
+#   - filter (re.sub?) non alpha-numeric from early data set
 #--------------------------------------------------------------------
 
 import csv, itertools
 
+#Prompt for file / count
+file_name = raw_input('File name? (omit .csv) ')
+list_count = input('How many positions are needed? (5, 10, 25, etc.) ')
+                   
 #Read the contents of 'readsample.csv' and begin splitting it
-incoming_data = open('readsample.csv', 'r').read().split()
+incoming_data = open(file_name + '.csv', 'r').read().split()
 delimited_data = [item.split(',') for item in incoming_data]
 
 #Recombine the contents of the list into one meta list
@@ -24,5 +27,5 @@ aux.sort()
 aux.reverse()
 
 #For the top 25 entries, print them
-for a in aux[:25]:
+for a in aux[:list_count]:
     print a
